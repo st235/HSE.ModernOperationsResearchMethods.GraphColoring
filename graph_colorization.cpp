655e5340cbe376af9c4d338890e8d55fba782848
Alexander Dadukin <hi@st235.xyz>
2023-01-16 23:06:11 +0000

    implemented rfl algorithm.

--- graph_colorization.cpp
+++ graph_colorization.cpp
@@ -4,11 +4,13 @@
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <limits>
 #include <set>
 #include <sstream>
 #include <string>
 #include <time.h>
 #include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
 namespace {
@@ -42,7 +44,7 @@ std::string ListToString(const std::vector<T>& vector, char delimiter = ' ') {
 class ColoringProblem {
 private:
     static constexpr int32_t kColorNoColor = -1;
-    static constexpr int32_t kColorInitial = 1;
+    static constexpr int32_t kColorInitial = 0;
 
     struct Node {
     public:
@@ -92,6 +94,18 @@ private:
         }
     };
 
+    struct NodeDegreeComparator {
+    public:
+        bool operator()(const std::pair<uint32_t, uint32_t>& lhs,
+                        const std::pair<uint32_t, uint32_t>& rhs) const {
+            if (lhs.second == rhs.second) {
+                return lhs.first > rhs.first;
+            }
+
+            return lhs.second > rhs.second;
+        }
+    };
+
     std::vector<int32_t> colors_;
     uint32_t max_color_ = static_cast<uint32_t>(kColorInitial);
     std::vector<std::unordered_set<uint32_t>> graph_;
@@ -137,7 +151,14 @@ public:
     }
 
     void GreedyGraphColoring() {
-        std::set<Node, NodeSaturationComparator> queue;
+        RFL();
+    }
+
+    // Recursive-Large-First algorithm
+    void RFL() {
+        int32_t ongoing_color = kColorInitial;
+        std::unordered_map<uint32_t, uint32_t> vertex_to_degree_lookup;
+        std::set<std::pair<uint32_t, uint32_t>, NodeDegreeComparator> queue;
 
         std::vector<uint32_t> vertices_degrees(graph_.size());
 
@@ -146,59 +167,157 @@ public:
         for (size_t i = 0; i < graph_.size(); i++) {
             const auto& adjacent_vertices = graph_[i];
 
-            // let's reset all colors to kColorNoColor
             colors_[i] = kColorNoColor;
-            vertices_degrees[i] = adjacent_vertices.size();
 
-            queue.insert(Node(static_cast<uint32_t>(i) /* id */, 
-                              static_cast<uint32_t>(adjacent_colors[i].size()) /* saturation */,
-                              vertices_degrees[i] /* uncolored_neighborhood_degree */ ));
+            uint32_t vertex = static_cast<uint32_t>(i);
+            uint32_t degree = adjacent_vertices.size();
+
+            queue.insert(std::make_pair(
+                vertex,
+                degree
+            ));
+
+            vertex_to_degree_lookup[vertex] = degree;
         }
 
         while (!queue.empty()) {
-            const auto queue_iterator = queue.begin();
-            Node node = *queue_iterator;
-            queue.erase(queue_iterator);
+            // std::cout << "iter: " << ongoing_color + 1 << std::endl;
+            const auto& iterator = queue.begin();
+            std::pair<uint32_t, uint32_t> node = *iterator;
+            queue.erase(node);
 
-            int32_t current_color = kColorNoColor;
-            std::vector<bool> available_colors(colors_.size(), true);
-            for (const auto& neighbour: graph_[node.id]) {
-                int32_t color = colors_[neighbour];
-                if (color != kColorNoColor) {
-                    available_colors[color] = false;
+            std::unordered_set<uint32_t> s;
+            std::unordered_set<uint32_t> adj_to_s;
+            std::unordered_set<uint32_t> uncolored_subset;
+
+            s.insert(node.first);
+            for (const auto& av: graph_[node.first]) {
+                adj_to_s.insert(av);
+            }
+            colors_[node.first] = ongoing_color;
+            // std::cout << "first: " << node.first << std::endl;
+
+            // add all uncolored vertices
+            for (uint32_t v = 0; v < graph_.size(); v++) {
+                bool is_adj_to_s = adj_to_s.find(v) != adj_to_s.end();
+                if (colors_[v] == kColorNoColor && !is_adj_to_s) {
+                    uncolored_subset.insert(v);
                 }
             }
-            for (size_t color = 0; color < available_colors.size(); color++) {
-                if (available_colors[color]) {
-                    current_color = color;
+
+            while (!uncolored_subset.empty()) {
+                int32_t y_vertex = -1;
+                int32_t yadj = std::numeric_limits<int32_t>::min();
+                int32_t ynadj = std::numeric_limits<int32_t>::max();
+
+                for (const auto& v: uncolored_subset) {
+                    int32_t adj_to_s_neighbours_count = 0;
+                    int32_t not_adj_to_s_neighbours_count = 0;
+
+                    for (const auto nv: graph_[v]) {
+                        if (adj_to_s.find(nv) != adj_to_s.end()) {
+                            adj_to_s_neighbours_count += 1;
+                        } else {
+                            not_adj_to_s_neighbours_count += 1;
+                        }
+                    }
+
+                    if ((adj_to_s_neighbours_count > yadj) || (adj_to_s_neighbours_count == yadj && not_adj_to_s_neighbours_count < ynadj)) {
+                        y_vertex = v;
+                        yadj = adj_to_s_neighbours_count;
+                        ynadj = not_adj_to_s_neighbours_count;
+                    }
+                }
+
+                // std::cout << "others: " << y_vertex << std::endl;
+
+                if (y_vertex == -1) {
                     break;
                 }
-            }
 
-            colors_[node.id] = current_color;
-            max_color_ = std::max(max_color_, static_cast<uint32_t>(current_color));
+                queue.erase(std::make_pair(
+                    y_vertex,
+                    vertex_to_degree_lookup[y_vertex]
+                ));
 
-            for (const auto& neighbour: graph_[node.id]) {
-                if (colors_[neighbour] != kColorNoColor) {
-                    continue;
+                s.insert(y_vertex);
+                uncolored_subset.erase(y_vertex);
+                for (const auto& av: graph_[y_vertex]) {
+                    adj_to_s.insert(av);
+                    uncolored_subset.erase(av);
                 }
-
-                Node old_neighbour_state(static_cast<uint32_t>(neighbour) /* id */, 
-                                         static_cast<uint32_t>(adjacent_colors[neighbour].size()) /* saturation */,
-                                         vertices_degrees[neighbour] /* uncolored_neighborhood_degree */ );
-
-                adjacent_colors[neighbour].insert(current_color);
-                vertices_degrees[neighbour] -= 1;
-                queue.erase(old_neighbour_state);
-
-                Node new_neighbour_state(static_cast<uint32_t>(neighbour) /* id */, 
-                                         static_cast<uint32_t>(adjacent_colors[neighbour].size()) /* saturation */,
-                                         vertices_degrees[neighbour] /* uncolored_neighborhood_degree */ );
-
-                queue.insert(new_neighbour_state);
+                colors_[y_vertex] = ongoing_color;
             }
+        
+            ongoing_color += 1;
         }
+
+        max_color_ = ongoing_color - 1;
     }
+
+    // void GreedyGraphColoring() {
+    //     std::set<Node, NodeSaturationComparator> queue;
+
+    //     std::vector<uint32_t> vertices_degrees(graph_.size());
+
+    //     std::vector<std::unordered_set<int32_t>> adjacent_colors(graph_.size());
+
+    //     for (size_t i = 0; i < graph_.size(); i++) {
+    //         const auto& adjacent_vertices = graph_[i];
+
+    //         // let's reset all colors to kColorNoColor
+    //         colors_[i] = kColorNoColor;
+    //         vertices_degrees[i] = adjacent_vertices.size();
+
+    //         queue.insert(Node(static_cast<uint32_t>(i) /* id */, 
+    //                           static_cast<uint32_t>(adjacent_colors[i].size()) /* saturation */,
+    //                           vertices_degrees[i] /* uncolored_neighborhood_degree */ ));
+    //     }
+
+    //     while (!queue.empty()) {
+    //         const auto queue_iterator = queue.begin();
+    //         Node node = *queue_iterator;
+    //         queue.erase(queue_iterator);
+
+    //         int32_t current_color = kColorNoColor;
+    //         std::vector<bool> available_colors(colors_.size(), true);
+    //         for (const auto& neighbour: graph_[node.id]) {
+    //             int32_t color = colors_[neighbour];
+    //             if (color != kColorNoColor) {
+    //                 available_colors[color] = false;
+    //             }
+    //         }
+    //         for (size_t color = 0; color < available_colors.size(); color++) {
+    //             if (available_colors[color]) {
+    //                 current_color = color;
+    //                 break;
+    //             }
+    //         }
+
+    //         colors_[node.id] = current_color;
+    //         max_color_ = std::max(max_color_, static_cast<uint32_t>(current_color));
+
+    //         for (const auto& neighbour: graph_[node.id]) {
+    //             if (colors_[neighbour] != kColorNoColor) {
+    //                 continue;
+    //             }
+
+    //             Node old_neighbour_state(static_cast<uint32_t>(neighbour) /* id */, 
+    //                                      static_cast<uint32_t>(adjacent_colors[neighbour].size()) /* saturation */,
+    //                                      vertices_degrees[neighbour] /* uncolored_neighborhood_degree */ );
+
+    //             adjacent_colors[neighbour].insert(current_color);
+    //             vertices_degrees[neighbour] -= 1;
+    //             queue.erase(old_neighbour_state);
+
+    //             Node new_neighbour_state(static_cast<uint32_t>(neighbour) /* id */, 
+    //                                      static_cast<uint32_t>(adjacent_colors[neighbour].size()) /* saturation */,
+    //                                      vertices_degrees[neighbour] /* uncolored_neighborhood_degree */ );
+
+    //             queue.insert(new_neighbour_state);
+    //         }
+    //     }
+    // }
 
     bool Check() {
         for (size_t i = 0; i < graph_.size(); ++i) {
